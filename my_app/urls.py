0fafee93bb7a2a4e2a8fd3c8b660c59a1057b4e6
IntelComputers <farmusic@inbox.ru>
2024-03-30 22:11:43 +0500

    viewslar ulandi

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,7 +1,32 @@
-
 from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('blog-details/', views.blog_details, name='blog_details'),
+    path('blog-left-sidebar/', views.blog_left_sidebar, name='blog_left_sidebar'),
+    path('blog-no-sidebar/', views.blog_no_sidebar, name='blog_no_sidebar'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
+    path('error404/', views.error404, name='error404'),
+    path('faq/', views.faq, name='faq'),
+    path('index-2/', views.index_2, name='index_2'),
+    path('index-box/', views.index_box, name='index_box'),
+    path('my-account/', views.my_account, name='my_account'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('portfolio-details/', views.portfolio_details, name='portfolio_details'),
+    path('portfolio-three-column/', views.portfolio_three_column, name='portfolio_three_column'),
+    path('shop-grid/', views.shop_grid, name='shop_grid'),
+    path('shop-left-sidebar/', views.shop_left_sidebar, name='shop_left_sidebar'),
+    path('shop-list/', views.shop_list, name='shop_list'),
+    path('shop-no-sidebar/', views.shop_no_sidebar, name='shop_no_sidebar'),
+    path('shop-right-sidebar/', views.shop_right_sidebar, name='shop_right_sidebar'),
+    path('single-product/', views.single_product, name='single_product'),
+    path('team/', views.team, name='team'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+
+
 
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -6,26 +6,70 @@ def index(request):
 def about(request):
     return render(request, 'my_app/about.html')
 
-def index(request):
-    return render(request, 'my_app/index.html')
+def blog_details(request):
+    return render(request, 'my_app/blog_details')
 
-def index(request):
-    return render(request, 'my_app/index.html')
+def blog_left_sidebar(request):
+    return render(request, 'my_app/blog-left-sidebar.html')
 
-def index(request):
-    return render(request, 'my_app/index.html')
+def blog_no_sidebar(request):
+    return render(request, 'my_app/blog-no-sidebar.html')
 
-def index(request):
-    return render(request, 'my_app/index.html')
 
-def index(request):
-    return render(request, 'my_app/index.html')
 
-def index(request):
-    return render(request, 'my_app/index.html')
+def cart(request):
+    return render(request, 'my_app/cart.html')
 
-def index(request):
-    return render(request, 'my_app/index.html')
+def checkout(request):
+    return render(request, 'my_app/checkout.html')
 
-def index(request):
-    return render(request, 'my_app/index.html')
+def contact(request):
+    return render(request, 'my_app/contact.html')
+
+def error404(request):
+    return render(request, 'my_app/error404.html')
+
+def faq(request):
+    return render(request, 'my_app/faq.html')
+
+def index_2(request):
+    return render(request, 'my_app/index-2.html')
+
+def index_box(request):
+    return render(request, 'my_app/index-box.html')
+
+def my_account(request):
+    return render(request, 'my_app/my-account.html')
+
+def portfolio(request):
+    return render(request, 'my_app/portfolio.html')
+
+def portfolio_details(request):
+    return render(request, 'my_app/portfolio-details.html')
+
+def portfolio_three_column(request):
+    return render(request, 'my_app/portfolio-three-column.html')
+
+def shop_grid(request):
+    return render(request, 'my_app/shop-grid.html')
+
+def shop_left_sidebar(request):
+    return render(request, 'my_app/shop-left-sidebar.html')
+
+def shop_list(request):
+    return render(request, 'my_app/shop-list.html')
+
+def shop_no_sidebar(request):
+    return render(request, 'my_app/shop-no-sidebar.html')
+
+def shop_right_sidebar(request):
+    return render(request, 'my_app/shop-right-sidebar.html')
+
+def single_product(request):
+    return render(request, 'my_app/single-product.html')
+
+def team(request):
+    return render(request, 'my_app/team.html')
+
+def wishlist(request):
+    return render(request, 'my_app/wishlist.html')
