0f19de80560510430654c6929bafe56a2bcc0e7e
IntelComputers <farmusic@inbox.ru>
2024-03-30 23:24:16 +0500

    linklar ulandi

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    name=models.CharField(max_length=200, verbose_name='Kitob nomi')
+    descrtiption=models.TextField(max_length=1000, verbose_name="Kitob haqida ma'lumot")
+    pdf=models.FileField(upload_to='books/pdf', verbose_name="Kitob faylini yuklash")
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
