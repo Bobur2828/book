c1485566b5f090d3c089811acd0b6cf6b4053eb5
IntelComputers <farmusic@inbox.ru>
2024-03-30 21:14:05 +0500

    первая фиксация

---
+++ my_app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return render(request, 'my_app/index.html')
+
+def about(request):
+    return render(request, 'my_app/about.html')
+
+def index(request):
+    return render(request, 'my_app/index.html')
+
+def index(request):
+    return render(request, 'my_app/index.html')
+
+def index(request):
+    return render(request, 'my_app/index.html')
+
+def index(request):
+    return render(request, 'my_app/index.html')
+
+def index(request):
+    return render(request, 'my_app/index.html')
+
+def index(request):
+    return render(request, 'my_app/index.html')
+
+def index(request):
+    return render(request, 'my_app/index.html')
+
+def index(request):
+    return render(request, 'my_app/index.html')
