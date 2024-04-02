300658408f6a71bde717eaaf2891a1c6c67ed28c
joraboyev04 <160149521+joraboyev04@users.noreply.github.com>
2024-04-02 17:56:48 +0500

    edit

--- my_app/forms.py
+++ my_app/forms.py
@@ -10,6 +10,8 @@ class AddCommentForm(forms.ModelForm):
 
 
 class CommentUpdateForm(forms.ModelForm):
+    comment = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'cols': 100}))
+    stars_given = forms.IntegerField(min_value=1, max_value=5)
     class Meta:
         model = Comment
         fields = ['comment','stars_given']
